e5549f238dda916295f8391e869bebee08cbdc30
sendtodwf-source <send.to.dwf@byui.edu>
2026-09-17 21:17:58 -0600

    My first Github push! yaaaay

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string first = "null";
+        string last = "null";
+        Console.Write("What is your first name? ");
+        first = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        last = Console.ReadLine();
+        Console.WriteLine($"Your name is {first}, {first} {last}.");
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int userGrade = -1;
+        string letter = "Null";
+        /*
+        A >= 90
+        B >= 80
+        C >= 70
+        D >= 60
+        F < 60
+        */
+        Console.WriteLine("This program is used to give grading, give a number between 0-100");
+        string userGradeStr= Console.ReadLine();
+        userGrade = int.Parse(userGradeStr);
+
+        if (userGrade >= 90)
+        {
+            Console.WriteLine("A");
+            letter = "A";
+
+        }
+        else if (userGrade >= 80)
+        {
+            Console.WriteLine("B");
+            letter = "B";
+
+        }
+        else if (userGrade >= 70)
+        {
+            Console.WriteLine("C");
+            letter = "C";
+
+        }
+        else if (userGrade >= 60)
+        {
+            Console.WriteLine("D");
+            letter = "D";
+
+        }
+        else if (userGrade < 60)
+        {
+            Console.WriteLine("man I'm so sorry you got an F");
+            letter = "F";
+        }
+        else
+        {
+            Console.WriteLine("Restart and enter a valid grade");
+        }
+        
+        if (userGrade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course!");
+        }
+        Console.WriteLine(letter);
+
     }
 }
